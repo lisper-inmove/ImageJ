73b38041f1c6707fcee8de0ae20557a86d3c676b
inmove <lisper.inmove@gmail.com>
2025-09-07 21:21:05 +0800

    Base

--- include/frames/MainFrame.h
+++ include/frames/MainFrame.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <QWidget>
 
 class ImageCanvas;
@@ -8,17 +8,8 @@ class MainFrame : public QWidget {
     Q_OBJECT
 public:
     explicit MainFrame(QWidget* parent = nullptr);
-    int add(int a, int b);
 
 private:
     void buildUi();
     void connectSignals();
-
-private slots:
-    void showHistogram();
-    void onMouseChange(const QPoint& wPos, const QPointF& iPos, bool inside);
-
-private:
-    ImageCanvas*  canvas_{nullptr};
-    RightSidebar* sidebar_{nullptr};
 };
--- tests/main_frame_test.cpp
+++ tests/main_frame_test.cpp
@@ -1,12 +1,10 @@
-#include <gtest/gtest.h>
+﻿#include <gtest/gtest.h>
 
 #include <QApplication>
 #include <QTest>
 #include <QTimer>
 
 #include "frames/MainFrame.h"
-#include "image_canvas/ImageCanvas.h"
-#include "test_header.h"
 
 TEST(UiSmoke, MainFrameShowsAndHides) {
     MainFrame w;
@@ -20,34 +18,4 @@ TEST(UiSmoke, MainFrameShowsAndHides) {
     w.hide();
     QTest::qWait(10);
     EXPECT_FALSE(w.isVisible());
-}
-
-TEST(UiSmoke, Croppe) {
-    MainFrame w;
-    w.show();
-    QTest::qWaitForWindowExposed(&w);
-    auto canvas = w.findChild<ImageCanvas*>("ImageCanvas");
-    ASSERT_NE(canvas, nullptr);
-
-    const QString& filepath= makeTempPng(80, 40);
-    canvas->openImageFromPath(filepath);
-    QTest::qWait(500);
-
-    QTest::mouseMove(canvas, QPoint(50, 50));
-    canvas->setFocus();
-
-    QTest::qWait(500);
-    QTest::keyPress(canvas, Qt::Key_Control);
-    QTest::mousePress(canvas, Qt::LeftButton);
-
-    QTest::qWait(500);
-    QTest::mouseMove(canvas, QPoint(90, 90));
-    QTest::qWait(500);
-
-    QTest::mouseRelease(canvas, Qt::LeftButton);
-    QTest::keyRelease(canvas, Qt::Key_Control);
-
-    // Optional: add any necessary waits if the GUI updates after this action
-    QTest::qWait(100);  // Add appropriate wait time if needed
-
 }
